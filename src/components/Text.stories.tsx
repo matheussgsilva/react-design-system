5c29e4bc055831b09bac1db9b966d312ae628d82
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-12 22:12:53 -0300

    Finsh the Text component and Stories

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,33 @@
-import { Text } from "./Text";
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from "./Text";
 
 export default {
   title: 'Component/Text',
   component: Text,
+  args: {
+    children: 'Lorem ipsum.'
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
 }
 
-export const Default = {}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  
+  }
+}
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Test</p>
+    )
+  }
+}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,9 +1,28 @@
-interface TextProps {
-  size?: 'sm' | 'md' | 'lg'
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
 }
 
-export function Text({ size = 'md' }: TextProps) {
+export function Text({ size = 'md', children, asChild }: TextProps) {
+  const Comp = asChild ? Slot : 'span';
+
   return (
-    <span className="text-gray-100 font-sans">Text</span>
+    <Comp 
+      className={clsx(
+        "text-gray-100 font-sans",
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        }
+      )}
+    >
+      {children}
+    </Comp>
   )
 }
