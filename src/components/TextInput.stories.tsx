3292c856af9f0d9f3d18a9d05233ebaf1851740d
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-13 19:17:49 -0300

    Create TextInput component and TextInput stories

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { TextInput, TextInputRootProps } from "./TextInput";
+
+export default {
+  title: 'Component/TextInput',
+  component: TextInput.Root,
+  args: {
+    children: (
+    <TextInput.Input placeholder="Type your e-mail address"/>
+    ),
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    },
+  }
+} as Meta<TextInputRootProps>
+
+export const Default: StoryObj<TextInputRootProps> = {}
+
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,31 @@
+import { clsx } from 'clsx';
+import { InputHTMLAttributes, ReactNode } from 'react';
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement>{
+}
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+  <div className='flex items-center gap-3 py-4 px-3 rounded bg-gray-800 outline-none w-full focus:ring-2 ring-cyan-300'>
+    {props.children}
+  </div>
+}
+
+//function TextInputIcon() {}
+
+function TextInputInput( props: TextInputInputProps) {
+  return (
+      <input 
+        className='bg-transparent flex-1 text-gray-10 text-xs placeholder:text-gray-400'
+        {...props}
+      />
+  )
+}
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+}
