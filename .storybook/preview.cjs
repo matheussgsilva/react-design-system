b401eb03721c3b650f9348cf5a5680e2f4358d41
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-14 11:42:27 -0300

    Add theme import in preview

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,4 +1,4 @@
-import { themes } from '@storybook/theming';
+import { themes } from '@storybook/theming'
 
 import '../src/styles/global.css';
 
@@ -12,5 +12,5 @@ export const parameters = {
   },
   docs: {
     theme: themes.dark,
-  }
+  },
 }
