06dbed29b050ee11d712c945c4c26871b8fe46bc
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-14 12:23:51 -0300

    Change config base in main.cjs

--- storybook-static/assets/preview.3dfca5c4.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.f9f19bc4.js";import"./iframe.0dabffa1.js";import"./es.map.constructor.f4c54ddb.js";import"./es.number.to-fixed.f8254d98.js";import"./index.51cc47d2.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.3dfca5c4.js.map
